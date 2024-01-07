383c5ad5c756a4648187205bf65f8d1e1e9f1807
MXJXN <modulations@gmail.com>
2024-01-07 09:01:27 -0500

    added warning

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ function App() {
   return (
     <>
       <div class="">
-        <h2 class="text-3xl font-bold pb-8">Zora Collection Holder Breakdown</h2>
+        <h2 class="text-3xl font-bold">Zora Collection Holder Breakdown</h2>
         <CollectorBalances />
+        <div class="text-md italic pt-4 pb-8">
+          🚨 Uses blockscout API, which sometimes misses transactions. To be
+          safe, check # Tokens and Total Collectors against Etherscan or OS
+          before using these addresses and numbers for airdrops, snapshots, etc.
+        </div>
       </div>
     </>
   );
